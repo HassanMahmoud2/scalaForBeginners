e76e3c699ea267c5bd3d14ee57f7a9bc7d0d1e3b
Hassan Mahmoud <hassan.abdelmageed@elmenus>
2024-01-11 12:33:29 +0200

    edited

--- src/main/scala/problemSolving/slidingSubarrayBeauty.scala
+++ src/main/scala/problemSolving/slidingSubarrayBeauty.scala
@@ -4,8 +4,6 @@ object slidingSubarrayBeauty extends App{
   def getSubarrayBeauty(nums: Array[Int], k: Int, x: Int): Array[Int] = {
     (nums.sliding(k).toArray.map(subarray => subarray.filter(_ < 0).sorted))
       .map(arr => arr.lift(x-1).getOrElse(0))
-
-
   }
   val nums = Array(-3, 1, 2, -3, 0, -3)
   //val nums = Array(1,-1,-3,-2,3)
